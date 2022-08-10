caf343c97e6cd5b127ffa15b0be0e10eb46efa31
Chaitanya Deshpande <deshpandech@vmware.com>
2022-08-10 18:06:28 +0530

    Update version.py

--- python/version.py
+++ python/version.py
@@ -7,8 +7,8 @@ Created on Apr 22, 2016
 '''
 import argparse
 
-AVI_VERSION = '22.1.1.post2'
-AVI_PIP_VERSION = '22.1.1.post2'
+AVI_VERSION = '22.1.1.post3'
+AVI_PIP_VERSION = '22.1.1.post3'
 
 if __name__ == '__main__':
     HELP_STR = '''
