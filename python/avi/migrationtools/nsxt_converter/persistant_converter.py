0ac809e0ecd0dcd03aa74f2c488491bdbbb4f5b6
Shardul Latkar <slatkar@vmware.com>
2022-10-27 19:41:59 +0530

    AV-159320: Added 2nd and 3rd phase scripts to pip package setup.py, Renamed scripts with nsxt prefix, Fixed persistence converter datascript issue

--- python/avi/migrationtools/nsxt_converter/persistant_converter.py
+++ python/avi/migrationtools/nsxt_converter/persistant_converter.py
@@ -270,7 +270,8 @@ class PersistantProfileConfigConv(object):
                 if lb_pp.get('cookie_time'):
                     cookie_max_idle = lb_pp['cookie_time'].get('cookie_max_idle', 0)
                     cookie_max_life = lb_pp['cookie_time'].get('cookie_max_life', 0)
-                script = "cookie_table={name=\"%s\",path=\"%s\",domain=\"%s\", \"%s\", \"%s\", \"%s\"}" \
+                script = "cookie_table={name=\"%s\",path=\"%s\",domain=\"%s\"," \
+                         "expires=\"%s\",maxage=\"%s\",httponly=\"%s\"}" \
                          " avi.http.add_cookie(cookie_table)" \
                          % (lb_pp.get('cookie_name'), lb_pp.get('cookie_path', '/'), lb_pp.get('cookie_domain'),
                             cookie_max_idle, cookie_max_life, lb_pp.get('cookie_httponly', False))
