5b10df1088e708a03b7a1f69ab4030d64e448ae7
Sagar <ssalvi@vmware.com>
2023-06-29 22:11:57 -0700

    Updated assets for java
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- java/src/com/vmware/avi/sdk/AviApi.java
+++ java/src/com/vmware/avi/sdk/AviApi.java
@@ -483,12 +483,11 @@ public class AviApi {
 	 * 
 	 * This method upload the file into the controller.
 	 * 
-	 * @param uri           is the api which upload a file
-	 * @param filePath      is file which we want to upload
-	 * @param fileUploadUri is uri where we have to upload file
+	 * @param uri           is the URL for file upload e.g fileservice or image
+	 * @param filePath      is file path which we want to upload from local
 	 * @throws Exception
 	 */
-	public void fileUpload(String uri, String filePath, String fileUploadUri) throws Exception {
+	public void fileUpload(String uri, String filePath) throws Exception {
 		CloseableHttpClient httpClient = null;
 		try {
 			httpClient = AviRestUtils.buildHttpClient(this.aviCredentials);
