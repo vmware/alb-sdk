2cb95339182cf10fa03ef57336f057c6face1447
Sagar <ssalvi@vmware.com>
2023-07-31 01:52:12 -0700

    Updated assets for go models and clients
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- go/clients/avi_client.go
+++ go/clients/avi_client.go
@@ -113,6 +113,7 @@ type AviClient struct {
 	SecurityManagerData             *SecurityManagerDataClient
 	SecurityPolicy                  *SecurityPolicyClient
 	ServerAutoScalePolicy           *ServerAutoScalePolicyClient
+	ServiceAuthProfile              *ServiceAuthProfileClient
 	ServiceEngine                   *ServiceEngineClient
 	ServiceEngineGroup              *ServiceEngineGroupClient
 	ServiceEngineGroupInventory     *ServiceEngineGroupInventoryClient
@@ -271,6 +272,7 @@ func NewAviClient(host string, username string, options ...func(*session.AviSess
 	aviClient.SecurityManagerData = NewSecurityManagerDataClient(aviSession)
 	aviClient.SecurityPolicy = NewSecurityPolicyClient(aviSession)
 	aviClient.ServerAutoScalePolicy = NewServerAutoScalePolicyClient(aviSession)
+	aviClient.ServiceAuthProfile = NewServiceAuthProfileClient(aviSession)
 	aviClient.ServiceEngine = NewServiceEngineClient(aviSession)
 	aviClient.ServiceEngineGroup = NewServiceEngineGroupClient(aviSession)
 	aviClient.ServiceEngineGroupInventory = NewServiceEngineGroupInventoryClient(aviSession)
