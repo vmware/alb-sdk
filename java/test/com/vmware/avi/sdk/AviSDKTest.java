630cca3e2fabd458d1cdf69083ddcc236e722a58
Sagar Salvi <ssalvi@vmware.com>
2023-08-03 10:22:23 +0530

    Auto updated assets for java 22.1.5 (#2733)
    * Updated assets for java
    
    Signed-off-by: Sagar <ssalvi@vmware.com>
    
    * Updated assets for java
    
    Signed-off-by: Sagar <ssalvi@vmware.com>
    
    ---------
    
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- java/test/com/vmware/avi/sdk/AviSDKTest.java
+++ java/test/com/vmware/avi/sdk/AviSDKTest.java
@@ -146,7 +146,8 @@ public class AviSDKTest {
 	public void testPostFileUpload() throws Exception {
 		try {
 			AviApi serv = new AviApi(AviSDKTest.getCredsPassword());
-			serv.fileUpload("fileservice?uri=controller://hsmpackages&hsmtype=safenet", "/mnt/files/hsmpackages/safenet.tar");
+			serv.fileUpload("fileservice/hsmpackages?hsmtype=safenet", "/mnt/files/hsmpackages/safenet.tar",
+					"controller://hsmpackages");
 		} catch (AviApiException e) {
 			e.printStackTrace(System.err);
 		}
