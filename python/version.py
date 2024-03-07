023f8cf8c21cdceeaba20ff81c236d0e8d593df3
Mayur Khachane <khachanem@vmware.com>
2024-03-07 16:23:43 +0530

    Update version.py

--- python/version.py
+++ python/version.py
@@ -7,8 +7,8 @@ Created on Apr 22, 2016
 '''
 import argparse
 
-AVI_VERSION = '22.1.6b1'
-AVI_PIP_VERSION = '22.1.6b1'
+AVI_VERSION = '22.1.6'
+AVI_PIP_VERSION = '22.1.6'
 
 if __name__ == '__main__':
     HELP_STR = '''
