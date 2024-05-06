b38cf0a0dfafce47038c8bf28c6a526fb77ac1b9
Sagar <ssalvi@vmware.com>
2024-05-06 01:55:49 -0700

    Updated assets for python
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- python/avi/sdk/utils/ansible_utils.py
+++ python/avi/sdk/utils/ansible_utils.py
@@ -405,9 +405,7 @@ def avi_ansible_api(module, obj_type, sensitive_fields):
             token=api_context['csrftoken'],
             port=api_creds.port,
             session_id=api_context['session_id'],
-            csrftoken=api_context['csrftoken'],
-            ssl_cert=api_context['ssl_cert'],
-            ssl_key=api_context['ssl_key'])
+            csrftoken=api_context['csrftoken'])
     else:
         api = ApiSession.get_session(
             api_creds.controller,
