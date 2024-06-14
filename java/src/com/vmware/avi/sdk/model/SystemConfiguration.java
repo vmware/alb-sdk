5bf9f3a0fbb2425552b8a6e691ee0737cb5c7135
Sagar <ssalvi@vmware.com>
2024-06-14 01:55:37 -0700

    Updated assets for java
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- java/src/com/vmware/avi/sdk/model/SystemConfiguration.java
+++ java/src/com/vmware/avi/sdk/model/SystemConfiguration.java
@@ -340,7 +340,7 @@ public class SystemConfiguration extends AviRestResource  {
     /**
      * This is the getter method this will return the attribute value.
      * Host header check.
-     * Field introduced in 31.1.1.
+     * Field introduced in 30.2.2.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as false.
      * @return enableHostHeaderCheck
@@ -352,7 +352,7 @@ public class SystemConfiguration extends AviRestResource  {
     /**
      * This is the setter method to the attribute.
      * Host header check.
-     * Field introduced in 31.1.1.
+     * Field introduced in 30.2.2.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as false.
      * @param enableHostHeaderCheck set the enableHostHeaderCheck.
@@ -733,7 +733,8 @@ public class SystemConfiguration extends AviRestResource  {
      * Trusted host profiles for host header validation.
      * Only function when enable_host_header_check is set to true.
      * It is a reference to an object of type trustedhostprofile.
-     * Field introduced in 31.1.1.
+     * Field introduced in 30.2.2.
+     * Maximum of 20 items allowed.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @return trustedHostProfilesRefs
@@ -747,7 +748,8 @@ public class SystemConfiguration extends AviRestResource  {
      * Trusted host profiles for host header validation.
      * Only function when enable_host_header_check is set to true.
      * It is a reference to an object of type trustedhostprofile.
-     * Field introduced in 31.1.1.
+     * Field introduced in 30.2.2.
+     * Maximum of 20 items allowed.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @return trustedHostProfilesRefs
@@ -761,7 +763,8 @@ public class SystemConfiguration extends AviRestResource  {
      * Trusted host profiles for host header validation.
      * Only function when enable_host_header_check is set to true.
      * It is a reference to an object of type trustedhostprofile.
-     * Field introduced in 31.1.1.
+     * Field introduced in 30.2.2.
+     * Maximum of 20 items allowed.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @return trustedHostProfilesRefs
--- java/src/com/vmware/avi/sdk/model/TrustedHostProfile.java
+++ java/src/com/vmware/avi/sdk/model/TrustedHostProfile.java
@@ -21,8 +21,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TrustedHostProfile extends AviRestResource  {
-    @JsonProperty("host_list")
-    private String hostList;
+    @JsonProperty("hosts")
+    private List<String> hosts;
 
     @JsonProperty("name")
     private String name;
@@ -37,35 +37,56 @@ public class TrustedHostProfile extends AviRestResource  {
     private String uuid;
 
 
-
     /**
      * This is the getter method this will return the attribute value.
-     * Comma separated list of host ip(v4/v6) addresses or fqdns.
-     * Field introduced in 31.1.1.
+     * List of host ip(v4/v6) addresses or fqdns.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
+     * Minimum of 1 items required.
+     * Maximum of 20 items allowed.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
-     * @return hostList
+     * @return hosts
      */
-    public String getHostList() {
-        return hostList;
+    public List<String> getHosts() {
+        return hosts;
     }
 
     /**
-     * This is the setter method to the attribute.
-     * Comma separated list of host ip(v4/v6) addresses or fqdns.
-     * Field introduced in 31.1.1.
+     * This is the setter method. this will set the hosts
+     * List of host ip(v4/v6) addresses or fqdns.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
+     * Minimum of 1 items required.
+     * Maximum of 20 items allowed.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
-     * @param hostList set the hostList.
+     * @return hosts
      */
-    public void setHostList(String  hostList) {
-        this.hostList = hostList;
+    public void setHosts(List<String>  hosts) {
+        this.hosts = hosts;
+    }
+
+    /**
+     * This is the setter method this will set the hosts
+     * List of host ip(v4/v6) addresses or fqdns.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
+     * Minimum of 1 items required.
+     * Maximum of 20 items allowed.
+     * Allowed in enterprise edition with any value, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return hosts
+     */
+    public TrustedHostProfile addHostsItem(String hostsItem) {
+      if (this.hosts == null) {
+        this.hosts = new ArrayList<String>();
+      }
+      this.hosts.add(hostsItem);
+      return this;
     }
 
     /**
      * This is the getter method this will return the attribute value.
      * Trustedhostprofile name.
-     * Field introduced in 31.1.1.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @return name
@@ -77,7 +98,7 @@ public class TrustedHostProfile extends AviRestResource  {
     /**
      * This is the setter method to the attribute.
      * Trustedhostprofile name.
-     * Field introduced in 31.1.1.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @param name set the name.
@@ -90,7 +111,7 @@ public class TrustedHostProfile extends AviRestResource  {
      * This is the getter method this will return the attribute value.
      * Tenant ref for trusted host profile.
      * It is a reference to an object of type tenant.
-     * Field introduced in 31.1.1.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @return tenantRef
@@ -103,7 +124,7 @@ public class TrustedHostProfile extends AviRestResource  {
      * This is the setter method to the attribute.
      * Tenant ref for trusted host profile.
      * It is a reference to an object of type tenant.
-     * Field introduced in 31.1.1.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @param tenantRef set the tenantRef.
@@ -132,7 +153,7 @@ public class TrustedHostProfile extends AviRestResource  {
     /**
      * This is the getter method this will return the attribute value.
      * Trustedhostprofile uuid.
-     * Field introduced in 31.1.1.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @return uuid
@@ -144,7 +165,7 @@ public class TrustedHostProfile extends AviRestResource  {
     /**
      * This is the setter method to the attribute.
      * Trustedhostprofile uuid.
-     * Field introduced in 31.1.1.
+     * Field introduced in 22.1.7, 30.2.2, 31.1.1.
      * Allowed in enterprise edition with any value, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as null.
      * @param uuid set the uuid.
@@ -165,7 +186,7 @@ public class TrustedHostProfile extends AviRestResource  {
       TrustedHostProfile objTrustedHostProfile = (TrustedHostProfile) o;
       return   Objects.equals(this.uuid, objTrustedHostProfile.uuid)&&
   Objects.equals(this.name, objTrustedHostProfile.name)&&
-  Objects.equals(this.hostList, objTrustedHostProfile.hostList)&&
+  Objects.equals(this.hosts, objTrustedHostProfile.hosts)&&
   Objects.equals(this.tenantRef, objTrustedHostProfile.tenantRef);
     }
 
@@ -173,7 +194,7 @@ public class TrustedHostProfile extends AviRestResource  {
     public String toString() {
       StringBuilder sb = new StringBuilder();
       sb.append("class TrustedHostProfile {\n");
-                  sb.append("    hostList: ").append(toIndentedString(hostList)).append("\n");
+                  sb.append("    hosts: ").append(toIndentedString(hosts)).append("\n");
                         sb.append("    name: ").append(toIndentedString(name)).append("\n");
                         sb.append("    tenantRef: ").append(toIndentedString(tenantRef)).append("\n");
                                     sb.append("    uuid: ").append(toIndentedString(uuid)).append("\n");
