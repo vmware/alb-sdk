913071e4ecde6ea734675dc5d6521c8492799b67
Sagar <ssalvi@vmware.com>
2024-07-03 01:56:21 -0700

    Updated assets for go models and clients
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- go/models/controller_sizing_limits.go
+++ go/models/controller_sizing_limits.go
@@ -20,6 +20,9 @@ type ControllerSizingLimits struct {
 	// Maximum number of east-west virtualservices. Field introduced in 20.1.1. Allowed in Enterprise edition with any value, Essentials, Basic, Enterprise with Cloud Services edition.
 	NumEastWestVirtualservices *int32 `json:"num_east_west_virtualservices,omitempty"`
 
+	// Maximum number of pools with realtime metrics enabled. Field introduced in 31.1.1. Allowed in Enterprise edition with any value, Enterprise with Cloud Services edition.
+	NumPoolRtMetrics *int32 `json:"num_pool_rt_metrics,omitempty"`
+
 	// Maximum number of servers. Field introduced in 20.1.1. Allowed in Enterprise edition with any value, Essentials, Basic, Enterprise with Cloud Services edition.
 	NumServers *int32 `json:"num_servers,omitempty"`
 
