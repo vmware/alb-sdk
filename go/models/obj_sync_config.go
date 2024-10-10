322d730ceae0ee0cb88970bf86235ffc89dd2535
Rohan <ssalvi@vmware.com>
2024-10-10 01:51:56 -0700

    Updated assets for go models and clients
    Signed-off-by: Rohan <ssalvi@vmware.com>

--- go/models/obj_sync_config.go
+++ go/models/obj_sync_config.go
@@ -8,7 +8,7 @@ package models
 // swagger:model ObjSyncConfig
 type ObjSyncConfig struct {
 
-	// SE CPU limit for InterSE Object Distribution. Allowed values are 15-80. Field introduced in 20.1.3. Unit is PERCENT. Allowed in Enterprise edition with any value, Enterprise with Cloud Services edition.
+	// SE CPU limit for InterSE Object Distribution. Allowed values are 0-100. Special values are 0- No Restriction.. Field introduced in 20.1.3. Unit is PERCENT. Allowed in Enterprise edition with any value, Enterprise with Cloud Services edition.
 	ObjsyncCPULimit *uint32 `json:"objsync_cpu_limit,omitempty"`
 
 	// Hub election interval for InterSE Object Distribution. Allowed values are 30-300. Field introduced in 20.1.3. Unit is SEC. Allowed in Enterprise edition with any value, Enterprise with Cloud Services edition.
