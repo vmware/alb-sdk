7c06b8cad9a432f3ae6def7adb6d888be39d8ed9
gurunath-avi <gurunath-reddy.kambala@broadcom.com>
2025-02-25 09:01:22 +0000

    Updated assets for java
    Signed-off-by: gurunath-avi <gurunath-reddy.kambala@broadcom.com>

--- java/src/com/vmware/avi/sdk/model/TCPProxyProfile.java
+++ java/src/com/vmware/avi/sdk/model/TCPProxyProfile.java
@@ -39,6 +39,9 @@ public class TCPProxyProfile  {
     @JsonProperty("congestion_recovery_scaling_factor")
     private Integer congestionRecoveryScalingFactor = 2;
 
+    @JsonProperty("delayed_ack_mode")
+    private Boolean delayedAckMode;
+
     @JsonProperty("delayed_ack_timer_delay")
     private Integer delayedAckTimerDelay = 100;
 
@@ -236,6 +239,30 @@ public class TCPProxyProfile  {
      */
     public void setCongestionRecoveryScalingFactor(Integer  congestionRecoveryScalingFactor) {
         this.congestionRecoveryScalingFactor = congestionRecoveryScalingFactor;
+    }
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Determines if delayed ack mode is enabledtrue  unconditionally use delayed ackfalse  don't use delayed ack.
+     * Field introduced in 31.1.2.
+     * Allowed with any value in enterprise, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return delayedAckMode
+     */
+    public Boolean getDelayedAckMode() {
+        return delayedAckMode;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Determines if delayed ack mode is enabledtrue  unconditionally use delayed ackfalse  don't use delayed ack.
+     * Field introduced in 31.1.2.
+     * Allowed with any value in enterprise, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param delayedAckMode set the delayedAckMode.
+     */
+    public void setDelayedAckMode(Boolean  delayedAckMode) {
+        this.delayedAckMode = delayedAckMode;
     }
 
     /**
@@ -724,7 +751,8 @@ public class TCPProxyProfile  {
   Objects.equals(this.keepaliveInHalfcloseState, objTCPProxyProfile.keepaliveInHalfcloseState)&&
   Objects.equals(this.autoWindowGrowth, objTCPProxyProfile.autoWindowGrowth)&&
   Objects.equals(this.ackOnPush, objTCPProxyProfile.ackOnPush)&&
-  Objects.equals(this.delayedAckTimerDelay, objTCPProxyProfile.delayedAckTimerDelay);
+  Objects.equals(this.delayedAckTimerDelay, objTCPProxyProfile.delayedAckTimerDelay)&&
+  Objects.equals(this.delayedAckMode, objTCPProxyProfile.delayedAckMode);
     }
 
     @Override
@@ -737,6 +765,7 @@ public class TCPProxyProfile  {
                         sb.append("    automatic: ").append(toIndentedString(automatic)).append("\n");
                         sb.append("    ccAlgo: ").append(toIndentedString(ccAlgo)).append("\n");
                         sb.append("    congestionRecoveryScalingFactor: ").append(toIndentedString(congestionRecoveryScalingFactor)).append("\n");
+                        sb.append("    delayedAckMode: ").append(toIndentedString(delayedAckMode)).append("\n");
                         sb.append("    delayedAckTimerDelay: ").append(toIndentedString(delayedAckTimerDelay)).append("\n");
                         sb.append("    idleConnectionTimeout: ").append(toIndentedString(idleConnectionTimeout)).append("\n");
                         sb.append("    idleConnectionType: ").append(toIndentedString(idleConnectionType)).append("\n");
