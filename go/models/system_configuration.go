58dda7ee052cf56d6aee99ad347f9bff17ad1526
avijenkins <gurunath-reddy.kambala@broadcom.com>
2025-07-15 09:00:13 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <gurunath-reddy.kambala@broadcom.com>

--- go/models/system_configuration.go
+++ go/models/system_configuration.go
@@ -108,7 +108,7 @@ type SystemConfiguration struct {
 	// Ability to sync the syslog server config to SEs. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
 	SyncSyslogToSe *bool `json:"sync_syslog_to_se,omitempty"`
 
-	// The destination Syslog server IP(v4/v6) address or FQDN. Provide in the format IP port. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
+	// The destination Syslog server IP(v4/v6) address or FQDN. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
 	SyslogServers []*IPAddr `json:"syslog_servers,omitempty"`
 
 	// Telemetry configuration. Field introduced in 31.1.1. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
