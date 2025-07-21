360a2754c0c4cb8d727d922573eb3627afe5ea2b
avijenkins <gurunath-reddy.kambala@broadcom.com>
2025-07-21 09:01:30 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <gurunath-reddy.kambala@broadcom.com>

--- go/models/archive_policy.go
+++ go/models/archive_policy.go
@@ -9,7 +9,7 @@ package models
 type ArchivePolicy struct {
 
 	// Specify a file path to add archive rule. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
-	Source *string `json:"source,omitempty"`
+	FilePath *string `json:"file_path,omitempty"`
 
 	// Specify a threshold for file path in MB. Field introduced in 31.2.1. Unit is MB. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
 	Threshold *uint32 `json:"threshold,omitempty"`
