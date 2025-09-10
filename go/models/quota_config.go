0b0658bb048bd2445a7a0306de6cbdab6fa55511
avijenkins <gurunath-reddy.kambala@broadcom.com>
2025-09-10 09:23:18 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <gurunath-reddy.kambala@broadcom.com>

--- go/models/quota_config.go
+++ go/models/quota_config.go
@@ -8,7 +8,7 @@ package models
 // swagger:model QuotaConfig
 type QuotaConfig struct {
 
-	// Maximum license service units allowed for consumption. -1 as Default is maximum value. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
+	// Maximum license service units allowed for consumption. -1 as Default is maximum value. Allowed values are -1-+4294967295. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
 	Limit *int64 `json:"limit,omitempty"`
 
 	// Minimum license service units reserved for consumption. Field introduced in 31.2.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
