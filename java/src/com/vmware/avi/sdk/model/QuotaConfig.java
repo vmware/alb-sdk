28980cd23706bddf5d6ae9daa80a4ff7e008a36b
avijenkins <svc.avi-jenkins@broadcom.com>
2025-09-12 09:00:32 +0000

    Updated assets for java
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- java/src/com/vmware/avi/sdk/model/QuotaConfig.java
+++ java/src/com/vmware/avi/sdk/model/QuotaConfig.java
@@ -33,7 +33,7 @@ public class QuotaConfig  {
      * This is the getter method this will return the attribute value.
      * Maximum license service units allowed for consumption.
      * -1 as default is maximum value.
-     * Allowed values are -1-+4294967295.
+     * Allowed values are -1-+65535.
      * Field introduced in 31.2.1.
      * Allowed with any value in enterprise, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as -1.
@@ -47,7 +47,7 @@ public class QuotaConfig  {
      * This is the setter method to the attribute.
      * Maximum license service units allowed for consumption.
      * -1 as default is maximum value.
-     * Allowed values are -1-+4294967295.
+     * Allowed values are -1-+65535.
      * Field introduced in 31.2.1.
      * Allowed with any value in enterprise, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as -1.
@@ -60,6 +60,7 @@ public class QuotaConfig  {
     /**
      * This is the getter method this will return the attribute value.
      * Minimum license service units reserved for consumption.
+     * Allowed values are 0-65535.
      * Field introduced in 31.2.1.
      * Allowed with any value in enterprise, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as 0.
@@ -72,6 +73,7 @@ public class QuotaConfig  {
     /**
      * This is the setter method to the attribute.
      * Minimum license service units reserved for consumption.
+     * Allowed values are 0-65535.
      * Field introduced in 31.2.1.
      * Allowed with any value in enterprise, enterprise with cloud services edition.
      * Default value when not specified in API or module is interpreted by Avi Controller as 0.
