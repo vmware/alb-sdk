eedeb7615c710a5a47ce0df36082d57c64bcc3ce
avijenkins <svc.avi-jenkins@broadcom.com>
2025-11-18 09:00:10 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- go/models/pre_checks_params.go
+++ go/models/pre_checks_params.go
@@ -8,8 +8,8 @@ package models
 // swagger:model PreChecksParams
 type PreChecksParams struct {
 
-	// Maximum wait time for configuration export to complete. Allowed values are 600-5400. Field introduced in 31.1.1. Unit is SEC. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
-	ExportConfigTimeout *uint32 `json:"export_config_timeout,omitempty"`
+	// Base timeout value for all upgrade pre-checks operations. The timeout value for applicable checks is a multiple of checks_base_timeout. For example, config export timeout = [multiplier] * checks_base_timeout. (The multiplier varies by operation.). Field introduced in 32.1.1. Unit is SEC. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
+	ChecksBaseTimeout *uint32 `json:"checks_base_timeout,omitempty"`
 
 	// Maximum number of alerts allowed for configuration export. Allowed values are 200-500. Field introduced in 31.1.1. Allowed with any value in Enterprise, Enterprise with Cloud Services edition.
 	MaxAlerts *uint32 `json:"max_alerts,omitempty"`
