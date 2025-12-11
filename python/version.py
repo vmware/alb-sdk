5372e17b5e63afb93dc12de16f696a89163f3b29
jpradeen <jpradeen@vmware.com>
2025-12-11 13:14:17 +0000

    Updating the version file with 30.2.7

--- python/version.py
+++ python/version.py
@@ -7,8 +7,8 @@ Created on Apr 22, 2016
 '''
 import argparse
 
-AVI_VERSION = '30.2.6'
-AVI_PIP_VERSION = '30.2.6'
+AVI_VERSION = '30.2.7'
+AVI_PIP_VERSION = '30.2.7'
 
 if __name__ == '__main__':
     HELP_STR = '''
