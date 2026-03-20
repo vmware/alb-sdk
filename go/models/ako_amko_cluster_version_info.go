23c5da96ab7ef14e821d2286e1d619755b86398f
avijenkins <svc.avi-jenkins@broadcom.com>
2026-03-20 09:00:58 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

---
+++ go/models/ako_amko_cluster_version_info.go
@@ -0,0 +1,16 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: Apache License 2.0
+package models
+
+// This file is auto-generated.
+
+// AkoAmkoClusterVersionInfo ako amko cluster version info
+// swagger:model AkoAmkoClusterVersionInfo
+type AkoAmkoClusterVersionInfo struct {
+
+	// AKO/AMKO operator version (e.g., '1.12.0'). Field introduced in 32.2.1. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
+	AkoAmkoVersion *string `json:"ako_amko_version,omitempty"`
+
+	// Kubernetes cluster version (e.g., '1.28.3'). Field introduced in 32.2.1. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
+	KubernetesVersion *string `json:"kubernetes_version,omitempty"`
+}
