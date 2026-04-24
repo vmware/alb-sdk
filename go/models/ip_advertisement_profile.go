7b2f8ff41fc6c6dd13002bbab65f0c463dec6542
avijenkins <svc.avi-jenkins@broadcom.com>
2026-04-24 09:07:18 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- go/models/ip_advertisement_profile.go
+++ go/models/ip_advertisement_profile.go
@@ -8,7 +8,7 @@ package models
 // swagger:model IpAdvertisementProfile
 type IPAdvertisementProfile struct {
 
-	// Default periodicity for periodic IP advertisement (GratARP/NA) in minutes. Used when a per-type periodicity is not specified. Allowed values are 5-30. Field introduced in 32.2.1. Unit is MIN. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
+	// Default periodicity for periodic IP advertisement (GratARP/NA) in minutes. Used when a per-type periodicity is not specified. Allowed values are 1-30. Field introduced in 32.2.1. Unit is MIN. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
 	DefaultPeriodicity *uint32 `json:"default_periodicity,omitempty"`
 
 	// List of IP address types for which periodic IP advertisement (GratARP/NA) is enabled. Supported ip_type values are VIP_IP, SNAT_IP, FLOATING_INTF_IP, and PRIMARY_INTF_IP. Applied uniformly to all VRFs in this ServiceEngineGroup. Field introduced in 32.2.1. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
