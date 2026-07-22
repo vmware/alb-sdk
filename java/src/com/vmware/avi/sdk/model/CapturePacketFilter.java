c7b8506653331a1d4f02f129031fc753ce8730e3
avijenkins <svc.avi-jenkins@broadcom.com>
2026-07-22 09:01:38 +0000

    Updated assets for java
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- java/src/com/vmware/avi/sdk/model/CapturePacketFilter.java
+++ java/src/com/vmware/avi/sdk/model/CapturePacketFilter.java
@@ -21,10 +21,42 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CapturePacketFilter  {
+    @JsonProperty("capture_ipc_filters")
+    private CaptureIPC captureIpcFilters;
+
     @JsonProperty("capture_tcp_filters")
     private List<CaptureTCPFilter> captureTcpFilters;
 
+    @JsonProperty("client_ip")
+    private DebugIpAddr clientIp;
 
+
+
+    /**
+     * This is the getter method this will return the attribute value.
+     * Capture filter for se ipc.
+     * Not applicable for debug virtual service.
+     * Field introduced in 32.2.1.
+     * Allowed with any value in enterprise, essentials, basic, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return captureIpcFilters
+     */
+    public CaptureIPC getCaptureIpcFilters() {
+        return captureIpcFilters;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Capture filter for se ipc.
+     * Not applicable for debug virtual service.
+     * Field introduced in 32.2.1.
+     * Allowed with any value in enterprise, essentials, basic, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param captureIpcFilters set the captureIpcFilters.
+     */
+    public void setCaptureIpcFilters(CaptureIPC captureIpcFilters) {
+        this.captureIpcFilters = captureIpcFilters;
+    }
     /**
      * This is the getter method this will return the attribute value.
      * Tcp params filter.
@@ -71,6 +103,32 @@ public class CapturePacketFilter  {
       return this;
     }
 
+    /**
+     * This is the getter method this will return the attribute value.
+     * Filters all packets of a complete transaction (client and server side), based on client ip.
+     * Supported for virtual service only.
+     * Field introduced in 32.2.1.
+     * Allowed with any value in enterprise, essentials, basic, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @return clientIp
+     */
+    public DebugIpAddr getClientIp() {
+        return clientIp;
+    }
+
+    /**
+     * This is the setter method to the attribute.
+     * Filters all packets of a complete transaction (client and server side), based on client ip.
+     * Supported for virtual service only.
+     * Field introduced in 32.2.1.
+     * Allowed with any value in enterprise, essentials, basic, enterprise with cloud services edition.
+     * Default value when not specified in API or module is interpreted by Avi Controller as null.
+     * @param clientIp set the clientIp.
+     */
+    public void setClientIp(DebugIpAddr clientIp) {
+        this.clientIp = clientIp;
+    }
+
 
     @Override
     public boolean equals(java.lang.Object o) {
@@ -81,14 +139,18 @@ public class CapturePacketFilter  {
           return false;
       }
       CapturePacketFilter objCapturePacketFilter = (CapturePacketFilter) o;
-      return   Objects.equals(this.captureTcpFilters, objCapturePacketFilter.captureTcpFilters);
+      return   Objects.equals(this.captureTcpFilters, objCapturePacketFilter.captureTcpFilters)&&
+  Objects.equals(this.captureIpcFilters, objCapturePacketFilter.captureIpcFilters)&&
+  Objects.equals(this.clientIp, objCapturePacketFilter.clientIp);
     }
 
     @Override
     public String toString() {
       StringBuilder sb = new StringBuilder();
       sb.append("class CapturePacketFilter {\n");
-                  sb.append("    captureTcpFilters: ").append(toIndentedString(captureTcpFilters)).append("\n");
+                  sb.append("    captureIpcFilters: ").append(toIndentedString(captureIpcFilters)).append("\n");
+                        sb.append("    captureTcpFilters: ").append(toIndentedString(captureTcpFilters)).append("\n");
+                        sb.append("    clientIp: ").append(toIndentedString(clientIp)).append("\n");
                   sb.append("}");
       return sb.toString();
     }
