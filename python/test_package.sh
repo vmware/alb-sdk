31a3b436f817173c9ec264bcd16586ff27b5c94e
avijenkins <svc.avi-jenkins@broadcom.com>
2026-09-01 09:01:55 +0000

    Updated assets for python
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- python/test_package.sh
+++ python/test_package.sh
@@ -2,13 +2,13 @@
 rm -rf dist/*
 sh create_sdk_pip_packages.sh sdk > test.log
 sh create_sdk_pip_packages.sh f5_converter > test.log
-virtualenv avi-sdk
+virtualenv -p python3.14 avi-sdk
 cd avi-sdk
 source bin/activate
 bin/pip install ../dist/avisdk-*.tar.gz > ../test.log
 bin/pip install ../dist/avif5converter-*.tar.gz > ../test.log
-python3 local/lib/python3.12/site-packages/avi/sdk/test/test_avi_api.py
-python3 local/lib/python3.12/site-packages/avi/f5_converter/test/test_f5_conversion_v11.py
-python3 local/lib/python3.12/site-packages/avi/f5_converter/test/test_f5_conversion_v10.py
+python3.14 local/lib/python3.14/site-packages/avi/sdk/test/test_avi_api.py
+python3.14 local/lib/python3.14/site-packages/avi/f5_converter/test/test_f5_conversion_v11.py
+python3.14 local/lib/python3.14/site-packages/avi/f5_converter/test/test_f5_conversion_v10.py
 cd ..
 rm -r avi-sdk
