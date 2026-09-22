3b0ea95b674dc627750c06450888a200710e3745
avijenkins <svc.avi-jenkins@broadcom.com>
2026-09-22 09:02:33 +0000

    Updated assets for go models and clients
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- go/models/l4_s_s_l_application_profile.go
+++ go/models/l4_s_s_l_application_profile.go
@@ -8,6 +8,9 @@ package models
 // swagger:model L4SSLApplicationProfile
 type L4SSLApplicationProfile struct {
 
+	// If enabled, the client's TLS fingerprint will be collected for this L4 SSL/TLS Virtual Service and made available to DataScripts via avi.ssl.get_tls_fingerprint(). Field introduced in 32.2.1. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
+	CollectClientTLSFingerprint *bool `json:"collect_client_tls_fingerprint,omitempty"`
+
 	// L4 stream idle connection timeout in seconds. Allowed values are 60-86400. Field introduced in 22.1.2. Unit is SEC. Allowed with any value in Enterprise, Essentials, Basic, Enterprise with Cloud Services edition.
 	SslStreamIdleTimeout *uint32 `json:"ssl_stream_idle_timeout,omitempty"`
 }
